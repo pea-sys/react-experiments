b8ca3cdb524e38eebc222cb6937a8207da9d4f98
pea-sys <pea98258@gmail.com>
2023-07-22 08:04:13 +0900

    データ

---
+++ ReactHandsOnLearning/ch8/parallel-requests/src/RepositoryReadme.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import ReactMarkdown from "react-markdown";
+
+export function useMountedRef() {
+  const mounted = useRef(false);
+  useEffect(() => {
+    mounted.current = true;
+    return () => (mounted.current = false);
+  });
+  return mounted;
+}
+
+export default function RepositoryReadme({ repo, login }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [markdown, setMarkdown] = useState("");
+
+  const mounted = useMountedRef();
+
+  const loadReadme = useCallback(async (login, repo) => {
+    setLoading(true);
+    const uri = `https://api.github.com/repos/${login}/${repo}/readme`;
+    const { download_url } = await fetch(uri).then((res) => res.json());
+    const markdown = await fetch(download_url).then((res) => res.text());
+    if (mounted.current) {
+      setMarkdown(markdown);
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!repo || !login) return;
+    loadReadme(login, repo).catch(setError);
+  }, [repo]);
+
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (loading) return <p>Loading...</p>;
+
+  return <ReactMarkdown source={markdown} />;
+}
