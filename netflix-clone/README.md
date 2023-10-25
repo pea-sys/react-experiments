524a45e82180df862e05bcd94b854ccced8fb96d
pea-sys <49807271+pea-sys@users.noreply.github.com>
2023-10-26 06:17:15 +0900

    feat : neflix-clone

---
+++ netflix-clone/README.md
@@ -0,0 +1,5 @@
+React で何かやりたいなということで手遊び  
+以下の記事を真似ただけです
+
+【React+TypeScript】Netflix のクローンを作るチュートリアル】
+https://zenn.dev/gunners6518/books/4c4672f32dd100
---
+++ netflix-clone/src/App.tsx
@@ -0,0 +1,27 @@
+import {requests} from './request'
+import { Row } from "./components/Row";
+import './App.css';
+import { Banner } from './components/Banner';
+import {Nav} from "./components/Nav"
+
+function App() {
+  return (
+<div className="App">
+  <Nav />
+  <Banner/>
+    <Row
+      title="NETFLIX ORIGUINALS"
+      fetchUrl={requests.feachNetflixOriginals}
+      isLargeRow
+    />
+    <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+    <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+    <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+    <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+    <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+    <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+</div>
+  );
+}
+
+export default App;
